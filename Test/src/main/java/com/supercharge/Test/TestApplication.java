03e17247a8003f28750210e1a6164606b4adba63
GaborMartin <gabormartin94@gmail.com>
2018-09-07 16:28:55 +0200

    Created basic objects (Account, Bank), printed out objects and bank account history.

--- Test/src/main/java/com/supercharge/Test/TestApplication.java
+++ Test/src/main/java/com/supercharge/Test/TestApplication.java
@@ -1,12 +1,17 @@
 package com.supercharge.Test;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.supercharge.Test.exception.NoTransactionsException;
+import com.supercharge.Test.exception.NotEnoughMoneyException;
+import com.supercharge.Test.model.BankAccount;
+import com.supercharge.Test.service.BankImpl;
 
-@SpringBootApplication
 public class TestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
+	public static void main(String[] args) throws NotEnoughMoneyException, NoTransactionsException {
+        BankAccount myAccount = new BankAccount("111-222-333", (long) 150000);
+        BankImpl bank = new BankImpl();
+        System.out.println(myAccount);
+        bank.depositMoney(myAccount, (long) 50000);
+        bank.printAccountHistory("none", myAccount);
+    }
 }
