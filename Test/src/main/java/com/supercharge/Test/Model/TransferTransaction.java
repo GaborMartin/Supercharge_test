3562d4e3dab16c42262c598d25b21f51d64e51ef
GaborMartin <gabormartin94@gmail.com>
2018-09-07 14:30:55 +0200

    Refactored transaction classes, fields and getters/setters has been deleted

--- Test/src/main/java/com/supercharge/Test/Model/TransferTransaction.java
+++ Test/src/main/java/com/supercharge/Test/Model/TransferTransaction.java
@@ -4,50 +4,7 @@ import java.util.Date;
 
 public class TransferTransaction extends Transaction {
 
-    private Date date;
-    private Double amount;
-    private Double balance;
-
     public TransferTransaction(Date date, Double amount, Double balance) {
         super(date, amount, balance);
-    }
-
-    @Override
-    public Date getDate() {
-        return date;
-    }
-
-    @Override
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    @Override
-    public Double getAmount() {
-        return amount;
-    }
-
-    @Override
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    @Override
-    public Double getBalance() {
-        return balance;
-    }
-
-    @Override
-    public void setBalance(Double balance) {
-        this.balance = balance;
-    }
-
-    @Override
-    public String toString() {
-        return "TransferTransaction{" +
-                "date=" + date +
-                ", amount=" + amount +
-                ", balance=" + balance +
-                '}';
     }
 }
