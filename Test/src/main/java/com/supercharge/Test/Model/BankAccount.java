61334299c3b6326a04cb54474842fdf4dcf44e11
GaborMartin <gabormartin94@gmail.com>
2018-09-07 14:34:04 +0200

    Field transactions has been added, provided getters/setters

--- Test/src/main/java/com/supercharge/Test/Model/BankAccount.java
+++ Test/src/main/java/com/supercharge/Test/Model/BankAccount.java
@@ -1,13 +1,18 @@
 package com.supercharge.Test.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BankAccount {
 
     private String accountNumber;
     private Double balance;
+    List<Transaction> transactions;
 
     public BankAccount(String accountNumber, Double balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
+        this.transactions = new ArrayList<>();
     }
 
     public String getAccountNumber() {
@@ -24,5 +29,13 @@ public class BankAccount {
 
     public void decreaseBalance(Double amount) {
         this.balance += amount;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
     }
 }
