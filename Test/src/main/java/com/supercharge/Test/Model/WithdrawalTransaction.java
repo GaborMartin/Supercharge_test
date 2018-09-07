53688f7b33aaf4cd7787c07eaf118cce357a6e5d
GaborMartin <gabormartin94@gmail.com>
2018-09-07 14:10:42 +0200

    Implemented basic class WithdrawalTransaction, added getters/setters

---
+++ Test/src/main/java/com/supercharge/Test/Model/WithdrawalTransaction.java
@@ -0,0 +1,54 @@
+package com.supercharge.Test.Model;
+
+import java.util.Date;
+
+public class WithdrawalTransaction extends Transaction {
+
+    private Date date;
+    private Double amount;
+    private Double balance;
+
+
+    public WithdrawalTransaction(Date date, Double amount, Double balance) {
+        super(date, amount, balance);
+    }
+
+    @Override
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public Double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public Double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "WithdrawalTransaction{" +
+                "date=" + date +
+                ", amount=" + amount +
+                ", balance=" + balance +
+                '}';
+    }
+}
